2e3abc6cbedf29150875bee589dffc3001927168
jensen-dong <jensendong141@gmail.com>
2024-06-22 14:54:13 -0700

    feat: finished adjusting tailwind css for better spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import WeatherForecast from "./components/WeatherForecast";
 
 const App = () => {
   return (
-    <div className="bg-slate-800 min-h-screen">
-      <h1 className="text-white text-center text-3xl p-4">Local Weather</h1>
-      <section className="flex justify-center">
-        <WeatherForecast />
-      </section>
+    <div className="bg-slate-800 min-h-screen flex items-center justify-center text-white">
+      <div className="text-center">
+        <h1 className="text-3xl p-4">Local Weather</h1>
+        <section className="flex justify-center">
+          <WeatherForecast />
+        </section>
+      </div>
     </div>
   );
 };
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,13 +1,13 @@
 const WeatherData = ({ forecast }) => {
   return (
     <div className="weather border border-gray-300 flex-1 p-2.5 text-center text-white">
-      <h2>{forecast.day}</h2>
+      <h2 className="mt-4">{forecast.day}</h2>
       <img src={forecast.img} alt={forecast.imgAlt} className="mx-auto"/>
-      <p>
+      <p className="my-4">
         <span className="font-bold">conditions: </span>
         {forecast.conditions}
       </p>
-      <p>
+      <p className="mb-4">
         <span className="font-bold">time: </span>
         {forecast.time}
       </p>
