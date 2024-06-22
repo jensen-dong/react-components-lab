b7acf06409e7876994ceef4d43a1068c4cff6c81
jensen-dong <jensendong141@gmail.com>
2024-06-22 15:01:25 -0700

    feat: destructure and create WeatherIcon component

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,8 +1,10 @@
+import WeatherIcon from "./WeatherIcon";
+
 const WeatherData = ({ forecast }) => {
   return (
     <div className="weather border border-gray-300 flex-1 p-2.5 text-center text-white">
       <h2 className="mt-4">{forecast.day}</h2>
-      <img src={forecast.img} alt={forecast.imgAlt} className="mx-auto"/>
+      <WeatherIcon src={forecast.img} alt={forecast.imgAlt} />
       <p className="my-4">
         <span className="font-bold">conditions: </span>
         {forecast.conditions}
---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,6 @@
+const WeatherIcon = ({ src, alt }) => {
+  return (
+    <img src={src} alt={alt} className="mx-auto mt-2"/>
+  )
+}
+export default WeatherIcon
